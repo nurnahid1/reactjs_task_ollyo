36ee892b0687b60fe8cb5067994e8fc2b5752dd7
Nur <hey@nurnahid.com>
2023-10-31 15:21:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <h2 className='underline text-cyan-500'>hello world!</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
